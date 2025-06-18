ee4036168306012805d0a5caecb62ca9ab1c3baf
nerdlab <nerdlab.kr@gmail.com>
2025-06-18 15:28:34 +0900

    feat: 개발/테스트 환경 구성

--- src/development.ts
+++ src/development.ts
@@ -1,13 +1,39 @@
+import { renderSidebar } from './components/sidebar';
+import { parseSearchUrl } from './utils/api';
+import { createTest } from './utils/testTemplate';
+
 /*
-  * This script is used for development purposes.
+  * This script is used for development.
   * For testing and debugging the extension.
  */
 
-console.log("🔥 Development script loaded");
-document.body.style.background = "#e3d3d3";
+console.log('🔥 Development script loaded');
+document.body.style.background = '#e3d3d3';
 
-chrome.runtime.sendMessage({ type: "RELOAD_EXTENSION" }).then(r => {
-  console.log("Extension reloaded:", r);
-}).catch(err => {
-  console.error("Error reloading extension:", err);
-});
+renderSidebar(document.body);
+
+createTest('testParseSearchUrl-1',
+  'https://poe.game.daum.net/trade2/search/poe2/Dawn%20of%20the%20Hunt/jDDwQ4XSX',
+  (value: string) => {
+    const result = parseSearchUrl(value);
+
+    if (result) {
+      return `Server: ${result.serverName}, ID: ${result.id}`;
+    } else {
+      return 'Invalid URL format';
+    }
+  }
+);
+
+createTest('testParseSearchUrl-2',
+  'https://www.pathofexile.com/trade2/search/poe2/Dawn%20of%20the%20Hunt/jDDwQ4XSX',
+  (value: string) => {
+    const result = parseSearchUrl(value);
+
+    if (result) {
+      return `Server: ${result.serverName}, ID: ${result.id}`;
+    } else {
+      return 'Invalid URL format';
+    }
+  }
+);
