60fb694daebc326461bc45eb455b6e856dd3b385
nerdlab <nerdlab.kr@gmail.com>
2025-06-28 15:58:45 +0900

    feat: 한국어 번역 기능 일부 개발 - 1

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,22 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // reload the extension when a development script sends a message
-  if (message.type === "RELOAD_EXTENSION") {
+  if (message.type === 'RELOAD_EXTENSION') {
     chrome.runtime.reload();
+  } else if (message.type === 'FETCH') {
+    fetch(message.url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        sendResponse({data: data.result});
+      })
+      .catch(error => {
+        sendResponse({ error: error.message });
+      });
+    // Indicate that the response will be sent asynchronously
+    return true;
   }
 });
---
+++ src/support/koreanTranslate.ts
@@ -0,0 +1,89 @@
+import { isKoreanServer, showToast } from '../utils/api';
+import * as cacheData from '../storage/cacheData';
+
+const LSCACHE_TRADE2ITEMS = 'https://poe.game.daum.net/api/trade2/data/items'
+const LSCACHE_TRADE2FILTERS = 'https://poe.game.daum.net/api/trade2/data/filters'
+const LSCACHE_TRADE2STATS = 'https://poe.game.daum.net/api/trade2/data/stats'
+const LSCACHE_TRADE2DATA = 'https://poe.game.daum.net/api/trade2/data/static';
+
+
+export async function applyKoreanTranslate() {
+  if(isKoreanServer()) {
+    return;
+  }
+
+  try {
+    await waitForDOM();
+    await translateLscaches();
+
+
+    showToast('Korean translation applied successfully!', '#0f0');
+  } catch (error) {
+    showToast('Error applying Korean translation. Please try again later.', '#f00');
+    console.error('Error applying Korean translation:', error);
+  }
+}
+
+
+// async function extractTranslationFromPage(): Promise<Record<string, string>> {
+//   const url = 'https://web.poecdn.com/js/translate.ko_KR.js';
+//   const res = await fetch(url);
+//
+//
+//   if (!res.ok) {
+//     throw new Error(`Failed to fetch translation script: ${res.status}`);
+//   }
+//
+//   const text = await res.text();
+//   const dict: Record<string, string> = {};
+//
+//   // 정규식: __['Key'] = 'Value';
+//   const regex = /__\[\s*['"](.+?)['"]\s*]\s*=\s*['"](.+?)['"]\s*;/g;
+//   let match;
+//   while ((match = regex.exec(text)) !== null) {
+//     const [, key, value] = match;
+//     dict[key] = value;
+//   }
+//
+//   return dict;
+// }
+
+
+async function translateLscaches() {
+  await setItem('lscache-trade2items', LSCACHE_TRADE2ITEMS);
+  await setItem('lscache-trade2filters', LSCACHE_TRADE2FILTERS);
+  await setItem('lscache-trade2stats', LSCACHE_TRADE2STATS);
+  await setItem('lscache-trade2data', LSCACHE_TRADE2DATA);
+}
+
+async function setItem(key: string, fetchUrl: string) {
+  const response = await cacheData.getOrFetchCache(key, 1000 * 60 * 60, () => fetchFromBackground(fetchUrl));
+  if (!response.data) {
+    console.error(response.error);
+    throw new Error(`Failed to fetch data from ${fetchUrl}`);
+  }
+
+  localStorage.setItem(key, JSON.stringify(response.data));
+}
+
+async function fetchFromBackground(url: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage({ type: 'FETCH', url }, (response) => {
+      if (chrome.runtime.lastError) {
+        reject(new Error(chrome.runtime.lastError.message));
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
+async function waitForDOM(): Promise<void> {
+  return new Promise((resolve) => {
+    if (document.readyState === "complete") {
+      resolve();
+    } else {
+      window.addEventListener("load", () => resolve(), { once: true });
+    }
+  });
+}
