e07b592632a27fe79200f751cd793eb90bd80863
nerdlab <nerdlab.kr@gmail.com>
2025-07-14 14:27:51 +0900

    fix(cotent): 세션이 만료될 경우 리다이랙션 되는 로그인페이지에서 sidebar를 랜더링 하지 않도록 변경

--- src/content-script.ts
+++ src/content-script.ts
@@ -8,12 +8,15 @@ const content = document.querySelector('.content') as HTMLElement;
 if (!content) {
   console.error(getMessage('error_content_element_not_found'));
 }
-
-
-Promise.resolve()
-  .then(() => renderSidebar(content))
-  .then(async () => {
-    // Butler Guide: 처음 실행 시 가이드 실행
-    await butlerGuide.runButlerGuides();
-  });
+if (content.querySelector('.backdrop')) {
+  // backdrop이 있을 경우 로그인 토큰이 만료되어 로그인 페이지로 리다이렉트된 상태
+  console.info(getMessage('info_backdrop_detected'));
+} else {
+  Promise.resolve()
+    .then(() => renderSidebar(content))
+    .then(async () => {
+      // Butler Guide: 처음 실행 시 가이드 실행
+      await butlerGuide.runButlerGuides();
+    });
+}
 
